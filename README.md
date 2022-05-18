6668fa7fcee38ce775225f9e29c522da7e356824
Biraj <biraj@pop-os.localdomain>
2022-05-18 22:25:16 +0530

    N-D support finally done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# ArrayVis
+
+![ArrayVis Screenshot](images/ss.png "ArrayVis Screenshot")
+
 A simple tool that can be used to visualize multi-dimensional arrays.
--- main.js
+++ main.js
@@ -4,9 +4,18 @@ const BG_COLOR = "#08090a";
 const BLUE = "#3ab1d2";
 const ORANGE = "#ffbc03";
 const PINK = "#c22eb8";
+const RED = "#C1554D";
+const GREEN = "#97B82B";
+const LIGHT_YELLOW = "#FFEDCB";
 
-const GAP = 8;
-const MATRIX_GAP = 12; // gap between matrices in a 3D array
+const colors = [LIGHT_YELLOW, RED, GREEN, PINK, BLUE];
+
+const LINE_WIDTH = 2;
+
+const GAP = 10;
+const MATRIX_GAP_X = 15; // gap between matrices in a 3D array
+const MATRIX_GAP_Y = 13; // gap between matrices in a 3D array
+const ND_SHAPE_GAP = 25 // gap between 3D - ND shapes
 const CELL_SIZE = 25;
 
 const canvas = document.getElementById("vis");
@@ -16,9 +25,6 @@ const draw_btn_el = document.getElementById("draw-btn");
 
 draw_btn_el.onclick = visualize;
 download_btn_el.onclick = download_image;
-
-canvas.width = 600;
-canvas.height = 600;
 
 const ctx = canvas.getContext("2d");
 
@@ -40,12 +46,15 @@ function visualize() {
         return;
     }
 
-    [canvas.width, canvas.height] = canvas_size(shape);
+    [canvas.width, canvas.height] = shape_size(shape);
+    canvas.width += 2 * GAP;
+    canvas.height += 2 * GAP;
+
     clear_canvas();
     draw(shape);
 }
 
-/*** UTILITY STUFF ***/
+/*** utility functions ***/
 function is_digit(c) {
     return c >= "0" && c <= "9";
 }
@@ -59,27 +68,39 @@ function parse_int(str) {
     return parseInt(str);
 }
 
-function canvas_size(shape) {
-    let width = 2 * GAP,
-        height = 2 * GAP;
+function shape_size(shape) {
+    const ndims = shape.length;
+    let width, height;
 
-    let n_mats, n_rows, n_cols;
-    switch (shape.length) {
-        case 1:
-            width += CELL_SIZE * shape[0] + (shape[0] - 1) * GAP;
-            height += CELL_SIZE;
-            break;
+    if (ndims == 1) {
+        width = CELL_SIZE * shape[0] + (shape[0] - 1) * GAP;
+        height = CELL_SIZE;
+    } else if (ndims == 2) {
+        const [n_rows, n_cols] = shape;
+        width = n_cols * CELL_SIZE + (shape[1] + 1) * GAP;
+        height = n_rows * CELL_SIZE + (n_rows + 1) * GAP;
+    } else if (ndims == 3) {
+        const [n_mats, n_rows, n_cols] = shape;
+        width = n_cols * CELL_SIZE + (n_cols + 3) * GAP + (n_mats - 1) * MATRIX_GAP_X;
+        height = n_rows * CELL_SIZE + (n_rows + 3) * GAP + (n_mats - 1) * MATRIX_GAP_Y;
+    } else {
+        [width, height] = shape_size(shape.slice(-3));
 
-        case 2:
-            [n_rows, n_cols] = shape;
-            width += 2 * GAP + CELL_SIZE * n_cols + (n_cols - 1) * GAP;
-            height += n_rows * CELL_SIZE + (n_rows + 1) * GAP;
-            break;
+        let horizontal = true;
+        for (let i = ndims - 4; i >= 0; --i) {
+            if (horizontal) {
+                width *= shape[i];
+                width += (shape[i] - 1) * ND_SHAPE_GAP;
+            } else {
+                height *= shape[i];
+                height += (shape[i] - 1) * ND_SHAPE_GAP;
+            }
 
-        case 3:
-            [n_mats, n_rows, n_cols] = shape;
-            width += 4 * GAP + n_cols * CELL_SIZE + (n_cols - 1) * GAP + (n_mats - 1) * MATRIX_GAP;
-            height += 2 * GAP + n_rows * CELL_SIZE + (n_rows + 1) * GAP + (n_mats - 1) * MATRIX_GAP;
+            width += 2 * GAP;
+            height += 2 * GAP;
+
+            horizontal = !horizontal;
+        }
     }
 
     return [width, height];
@@ -90,16 +111,21 @@ function clear_canvas() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-/*** DRAWING STUFF ***/
+function download_image(e) {
+    if (!image_created) {
+        e.preventDefault();
+        alert("Image is not yet created!");
+    }
+}
+
+/*** drawing functions ***/
 function draw(shape) {
     if (shape.length == 1)
-        draw_1d(shape[0]);
+        draw_1d(shape, GAP, GAP);
     else if (shape.length == 2)
-        draw_2d(shape[0], shape[1]);
-    else if (shape.length == 3)
-        draw_3d(shape[0], shape[1], shape[2])
+        draw_2d(shape, GAP, GAP);
     else
-        draw_ndim(shape);
+        draw_nd(shape, GAP, GAP);
 
     canvas.toBlob(blob => {
         download_btn_el.href = URL.createObjectURL(blob);
@@ -107,9 +133,12 @@ function draw(shape) {
     });
 }
 
-function draw_1d(len, x = GAP, y = GAP, bg_color = BG_COLOR) {
+function draw_1d(shape, x, y, bg_color = BG_COLOR) {
+    const [width, height] = shape_size(shape);
+    const [len] = shape;
+
     ctx.fillStyle = BLUE;
-    ctx.fillRect(x, y, CELL_SIZE * len + (len - 1) * GAP, CELL_SIZE);
+    ctx.fillRect(x, y, width, height);
 
     let gap_x = x + CELL_SIZE;
     ctx.fillStyle = bg_color;
@@ -119,66 +148,88 @@ function draw_1d(len, x = GAP, y = GAP, bg_color = BG_COLOR) {
     }
 }
 
-function draw_2d(n_rows, n_cols, x = GAP, y = GAP, complete = true) {
-    const matrix_width = 2 * GAP + n_cols * CELL_SIZE + (n_cols - 1) * GAP;
-    const matrix_height = n_rows * CELL_SIZE + (n_rows + 1) * GAP;
+function draw_2d(shape, x, y, draw_whole = true) {
+    const [width, height] = shape_size(shape);
+    const [n_rows, n_cols] = shape;
 
     ctx.fillStyle = BG_COLOR;
-    ctx.fillRect(x, y, matrix_width, matrix_height)
+    ctx.fillRect(x, y, width, height)
 
-    ctx.lineWidth = 2;
+    ctx.lineWidth = LINE_WIDTH;
     ctx.strokeStyle = PINK;
-    ctx.strokeRect(x, y, matrix_width, matrix_height);
-
-    x += GAP;
-    y += GAP;
-
-    if (complete) {
-        for (let i = 0; i < n_rows; ++i) {
-            draw_1d(n_cols, x, y);
-            y += CELL_SIZE + GAP;
-        }
-    } else {
-        draw_1d(n_cols, x, y);
-        y += CELL_SIZE + GAP;
-
-        for (let i = 1; i < n_rows; ++i) {
-            draw_1d(1, x, y);
-            y += CELL_SIZE + GAP;
-        }
-    }
-}
-
-function draw_3d(n_mats, n_rows, n_cols, x = GAP, y = GAP) {
-    let width = 4 * GAP + n_cols * CELL_SIZE + (n_cols - 1) * GAP + (n_mats - 1) * MATRIX_GAP;
-    let height = 2 * GAP + n_rows * CELL_SIZE + (n_rows + 1) * GAP + (n_mats - 1) * MATRIX_GAP;
-
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = ORANGE;
     ctx.strokeRect(x, y, width, height);
 
     x += GAP;
     y += GAP;
 
+    if (draw_whole) {
+        for (let i = 0; i < n_rows; ++i) {
+            draw_1d([n_cols], x, y);
+            y += CELL_SIZE + GAP;
+        }
+    } else {
+        draw_1d([n_cols], x, y);
+        y += CELL_SIZE + GAP;
+
+        for (let i = 1; i < n_rows; ++i) {
+            draw_1d([1], x, y);
+            y += CELL_SIZE + GAP;
+        }
+    }
+}
+
+function draw_3d(shape, x, y) {
+    const [width, height] = shape_size(shape);
+    const [n_mats, n_rows, n_cols] = shape;
+
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.strokeStyle = ORANGE;
+    ctx.strokeRect(x, y, width, height);
+
+    const shape_end_x = x + width,
+        shape_end_y = y + height;
+
+    x += GAP;
+    y += GAP;
+
     for (let i = 1; i < n_mats; ++i) {
-        draw_2d(n_rows, n_cols, x, y, false);
-        x += MATRIX_GAP;
-        y += MATRIX_GAP;
+        draw_2d([n_rows, n_cols], x, y, false);
+        x += MATRIX_GAP_X;
+        y += MATRIX_GAP_Y;
     }
 
-    draw_2d(n_rows, n_cols, x, y);
+    draw_2d([n_rows, n_cols], x, y);
+    return [shape_end_x, shape_end_y];
 }
 
-function draw_ndim(shape) {
-    let upper_dim_shape = shape.slice(0, -3);
-    alert("Not yet implemented!");
-}
+function draw_nd(shape, x, y) {
+    const ndims = shape.length;
+    if (ndims == 3)
+        return draw_3d(shape, x, y);
 
-function download_image(e) {
-    if (!image_created) {
-        e.preventDefault();
-        alert("Image is not yet created!");
+    let [width, height] = shape_size(shape);
+
+    ctx.strokeStyle = colors[(ndims - 3) % colors.length];
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.strokeRect(x, y, width, height);
+
+    const shape_end_x = x + width,
+        shape_end_y = y + height;
+
+    x += GAP;
+    y += GAP;
+
+    let horizontal = ndims % 2 == 0;
+    for (let i = 0; i < shape[0]; ++i) {
+        const [new_x, new_y] = draw_nd(shape.slice(1), x, y);
+
+        if (horizontal)
+            x = new_x + ND_SHAPE_GAP;
+        else
+            y = new_y + ND_SHAPE_GAP;
     }
+
+    return [shape_end_x, shape_end_y];
 }
 
 visualize();
